006cfb080d4aef5642fb0b538b402fac04019328
Ingrid Schmitdinger <ingrid.schmitdinger@gmail.com>
2023-02-16 21:26:11 +0100

    ok

--- App_Git.py
+++ App_Git.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import numpy as np
 
 st.write("# Novo aplicativo")
+
+st.write("#Novo Aplicativo")
+
+btn1 = st.button("Novo botao")
+
+if btn1 == True:
+    st.write("### Sua aplicacao esta funcionando!")
+
+l = np.random.randint(5,20,10)
+st.write(l)
