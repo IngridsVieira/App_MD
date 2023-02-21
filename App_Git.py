dd9e8494c83ab5156e3838cb6e7ef7efe613bd20
IngridsVieira <125602265+IngridsVieira@users.noreply.github.com>
2023-02-21 13:11:45 -0300

    Update App_Git.py

--- App_Git.py
+++ App_Git.py
@@ -24,5 +24,19 @@ if btn1 == True:
         
 st.title("Words Frequency!")
 
-txt = st.text_area('Digite um texto:')
+text = st.text_area('Digite um texto:')
 
+#cleaning
+text = text.lower()
+words = text.split()
+words = [word.strip('.,!;()[]') for word in words]
+words = [word.replace("'s", '') for word in words]
+
+#finding unique
+unique = []
+for word in words:
+    if word not in unique:
+        unique.append(word)
+
+#sort
+unique.sort()
