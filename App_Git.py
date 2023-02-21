460165ae688c3abc981933617d983efd8c151063
IngridsVieira <125602265+IngridsVieira@users.noreply.github.com>
2023-02-21 11:46:54 -0300

    Update App_Git.py

--- App_Git.py
+++ App_Git.py
@@ -22,15 +22,14 @@ Longitude = st.number_input('Longitude:',)
 
 if Latitude != "" and Longitude != "":
     st.write("Suas coordenadas são {} de Latitude e {} de Longitude".format(Latitude,Longitude))
+    
+btn1 = st.button("Gerar mapa:")
 
-st.write("### Sua localização no mapa:")
+if btn1 == True:
+    st.write("### Sua localização no mapa:")
+    if Latitude != "" and Longitude != "":
+        dic_map = {"lat": [Latitude],"lon": [Longitude]}
 
-
-if Latitude != "" and Longitude != "":
-    dic_map = {"lat": [Latitude],"lon": [Longitude]}
-
-    df_mapa = pd.DataFrame(dic_map)
-    st.map(df_mapa)
-else:
-    st.write("### Insira as suas coordenadas.")
+        df_mapa = pd.DataFrame(dic_map)
+        st.map(df_mapa)
 
